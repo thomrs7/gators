b1a47e2e972675aab223d2a4b33b8c1477da3b81
thom <thom.rs7@gmail.com>
2015-09-08 17:47:34 -0400

    Formatting Clean Summary

--- game_stats.R
+++ game_stats.R
@@ -20,27 +20,28 @@ plays <- raw_data[[46]]
 
 table  <- getNodeSet(htmlParse(doc),"//table") [[28]]  
 drives <- readHTMLTable(table,trim = TRUE, stringsAsFactors = FALSE,
-                    skip.rows=c(1),
-                    header=TRUE
+                    skip.rows=c(1),header=TRUE
                     )
 
-drives <- data.frame(lapply(drives, function(x) gsub('Â','',x)    ))
-
+drives <- data.frame(lapply(drives, function(x) gsub('Â','',x) ))
 names(drives) <- gsub('Â','',names(drives))
-
 drives$TOP <- as.duration(drives$TOP)
-
 
 ggplot(drives, aes(Team,TOP)) + geom_boxplot( aes(fill=Team)) + 
     geom_jitter( aes(shape=How.Lost), size=4, color='#FF4A00') +
     scale_fill_manual(values = c("#990000","#0021A5")) +
     scale_shape_manual(values = c(0,1,2,3,4,5,11)) + 
-    facet_wrap(~ Qtr,  ncol = 2) +
-    ggtitle("New Mexico State vs Florida (Sep 05, 2015)") +
-    ylab("Time of Position")
+    facet_wrap(~ Qtr,  ncol = 2) + ylab("Time of Position")
+    ggtitle("New Mexico State vs Florida (Sep 05, 2015)") 
 
-
-
+summary <- mutate(summary,
+                  V1 = gsub('Â','',V1),
+                  V2 = gsub('Â','',V2),
+                  V3 = gsub('Â','',V3),
+                  V4 = gsub('Â','',V4),
+                  V5 = gsub('Â','',V5)
+                  )
+    
 
 
 
